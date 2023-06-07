92d332b2ace6e74640c7372ce9baa7b5e7fc4ca6
tsukipedia <salma.rubiwabi@gmail.com>
2023-06-07 12:58:18 -0600

    add base url to controller

--- src/main/java/com/example/cats/Controller/CatsController.java
+++ src/main/java/com/example/cats/Controller/CatsController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.cats.Model.Cat;
 import com.example.cats.Service.CatService;
 
 @RestController
+@RequestMapping("/api/v1/cats/")
 public class CatsController {
 
     private CatService catService;
@@ -18,17 +20,17 @@ public class CatsController {
         this.catService = catService;
     }
 
-    @GetMapping("/api/cats/id/{id}")
+    @GetMapping("id/{id}")
     public Cat getCatById(@PathVariable String id) throws Exception {
         return catService.findById(id);
     }
 
-    @GetMapping("/api/cats/name/{name}")
+    @GetMapping("name/{name}")
     public Cat getCatByName(@PathVariable String name) throws Exception {
         return catService.findByName(name);
     }
 
-    @PostMapping("/api/cats")
+    @PostMapping()
     public Cat addCat(@RequestBody Cat cat) {
         return catService.createNewCat(cat);
     }
